72b8752adafb7f60eada1043b6b20a124b158257
Haroon Shah <haroon.shah@mail.utoronto.ca>
2024-09-16 11:45:51 -0400

    modified DataTypes.java

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -1,14 +1,24 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class DataTypes {
     // TODO TASK 1: fix this code so that it passes the test in DataTypesTest.java
     public static long sum(List<Integer> numbers) {
 
-        int s = 0;
+        long s = 0;
         // below is a "foreach" loop which iterates through numbers
         for (int x : numbers) {
             s += x;
         }
         return s;
+    }
+
+    public static void main(String[] args) {
+        long x = 500_000_500_000L;
+        List<Integer> lst = new ArrayList<>();
+        for (int i = 1; i <= 1_000_000; i++) {
+            lst.add(i);
+        }
+        System.out.println(sum(lst));
     }
 }
